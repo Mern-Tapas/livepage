6e468685107288217ad28d99998667b26c2b713d
Mern-Tapas <cyb6261452510@gmail.com>
2022-10-07 20:59:04 +0530

    first commite

---
+++ public/js/dashboard.js
@@ -0,0 +1,4 @@
+const navbutton = document.getElementById("navbutton")
+const sidebar = document.getElementById("sidebar")
+
+navbutton.addEventListener("click",()=>{sidebar.classList.toggle("sidebar-links-hide")})
---
+++ src/index.js
@@ -0,0 +1,34 @@
+require("dotenv").config()
+const { urlencoded } = require("body-parser")
+const express = require("express")
+require("./database")
+const hbs = require("hbs")
+const path = require("path")
+const app = express()
+
+const template = path.join(__dirname,"../template/view")
+const partials = path.join(__dirname,"../template/partials")
+
+
+app.set("view engine","hbs")
+app.set("views",template)
+hbs.registerPartials(partials)
+
+app.use(express.static("public"))
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+
+
+app.get("/",(req,res)=>{res.render("home")})
+app.get("/about",(req,res)=>{res.render("about")})
+app.get("/contact",(req,res)=>{res.render("contact")})
+app.get("/application",(req,res)=>{res.render("application")})
+app.get("/faculty",(req,res)=>{res.render("facultyregistration")})
+app.get("/login",(req,res)=>{res.render("login")})
+app.get("/dashboard",(req,res)=>{res.render("dashboard")})
+
+
+
+
+app.listen(process.env.PORT||4000,()=>{console.log("listening port 4000")})
